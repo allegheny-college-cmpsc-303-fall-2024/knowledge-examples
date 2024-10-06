8775e783de7bda1e462b97b2886379b2f618386a
howshekilledit <mgreen@allegheny.edu>
2024-10-06 13:44:40 -0400

    top40

--- puzzle.py
+++ puzzle.py
@@ -1,54 +1,60 @@
 from logic import *
 
-people = ["Gilderoy", "Pomona", "Minerva", "Horace"]
-houses = ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]
+# disclaimer! these are the four top 40 musicians when I googled it on Oct 6, 2024
+# i only know a couple of them, and i am not explicitly endorsing
+# any of their music or opinions
+# <3 professor green
+
+
+people = ["BillieEilish", "ChapelleRoan", "SabrinaCarpenter", "Shaboozey"]
+homestates = ["Pennsylvania", "Virginia", "Missouri", "California"]
 
 symbols = []
 
 knowledge = And()
 
 for person in people:
-    for house in houses:
-        symbols.append(Symbol(f"{person}{house}"))
+    for homestate in homestates:
+        symbols.append(Symbol(f"{person}{homestate}"))
 
-# Each person belongs to a house.
+# Each person belongs to a homestate.
 for person in people:
     knowledge.add(Or(
-        Symbol(f"{person}Gryffindor"),
-        Symbol(f"{person}Hufflepuff"),
-        Symbol(f"{person}Ravenclaw"),
-        Symbol(f"{person}Slytherin")
+        Symbol(f"{person}Pennsylvania"),
+        Symbol(f"{person}California"),
+        Symbol(f"{person}Virginia"),
+        Symbol(f"{person}Missouri")
     ))
 
-# Only one house per person.
+# Only one homestate per person.
 for person in people:
-    for h1 in houses:
-        for h2 in houses:
+    for h1 in homestates:
+        for h2 in homestates:
             if h1 != h2:
                 knowledge.add(
                     Implication(Symbol(f"{person}{h1}"), Not(Symbol(f"{person}{h2}")))
                 )
 
-# Only one person per house.
-for house in houses:
+# Only one person per homestate.
+for homestate in homestates:
     for p1 in people:
         for p2 in people:
             if p1 != p2:
                 knowledge.add(
-                    Implication(Symbol(f"{p1}{house}"), Not(Symbol(f"{p2}{house}")))
+                    Implication(Symbol(f"{p1}{homestate}"), Not(Symbol(f"{p2}{homestate}")))
                 )
 
 # Add some knowledge base for our AI to deduce upon
 knowledge.add(
-    Or(Symbol("GilderoyGryffindor"), Symbol("GilderoyRavenclaw"))
+    Or(Symbol("ShaboozeyPennsylvania"), Symbol("ShaboozeyVirginia"))
 )
 
 knowledge.add(
-    Not(Symbol("PomonaSlytherin"))
+    Not(Symbol("BillieEilishMissouri"))
 )
 
 knowledge.add(
-    Symbol("MinervaGryffindor")
+    Symbol("SabrinaCarpenterPennsylvania")
 )
 
 for symbol in symbols:
